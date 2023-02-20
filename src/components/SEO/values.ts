3fc5d8185a003fdae39dbb276c9e4958ac525f55
Nam Tran <namtrhg@gmail.com>
2023-02-20 10:43:52 +0700

    fix: update

--- src/components/SEO/values.ts
+++ src/components/SEO/values.ts
@@ -1,15 +1,21 @@
 export default {
     title: process.env.NEXT_PUBLIC_COMPANY_NAME,
-    description: `Chuyên cung cấp xe tải, ben giá rẻ | Liên hệ ${process.env.NEXT_PUBLIC_PHONE_NUMBER} (Mr. ${process.env.NEXT_PUBLIC_USER_NAME})`,
+    description: `Chuyên cung cấp xe tải, ben giá rẻ cùng nhiều ưu đãi hấp dẫn. 
+    Liên hệ ${process.env.NEXT_PUBLIC_PHONE_NUMBER} (Mr. ${process.env.NEXT_PUBLIC_USER_NAME})
+    Địa chỉ: ${process.env.NEXT_PUBLIC_COMPANY_ADDRESS}
+    Website:${process.env.NEXT_PUBLIC_DOMAIN}
+    Email: ${process.env.NEXT_PUBLIC_USER_EMAIL}
+    Rất hân hạn được phục vụ quý khách hàng!
+    `,
     indexable: false,
     openGraph: {
         type: 'website',
         locale: 'br_PT',
         host: process.env.NEXT_PUBLIC_DOMAIN,
-        title: 'Cars',
-        description: 'Search cars for sale.',
+        title: 'Kamaz',
+        description: 'Chuyên cung cấp xe tải giá rẻ',
         image: '',
-        site_name: 'Cars',
+        site_name: 'Kamaz',
     },
     twitter: {
         cardType: 'summary',
